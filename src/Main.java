df554fa6134de260e8bb105c18ac78991d9f5a5d
Ozge Yasayan <ozgey99@gmail.com>
2020-04-07 11:55:08 +0300

    Game implemented is in Main class.

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,23 @@ public class Main {
         ArrayList<Integer> result = a.concatAndSort(list1,list2,list3);
 
         System.out.println("Result => " + result.toString());
+        System.out.println("---------------------------------");
+
+        Player p1 = new Player("Tom");
+        Player p2 = new Player("John");
+        Player p3 = new Player("James");
+        Player p4 = new Player("Henry");
+
+        Die d = new Die();
+
+        for(int i = 0; i < 5; i++){
+            p1.addScore(d.rollDie());
+            p2.addScore(d.rollDie());
+            p3.addScore(d.rollDie());
+            p4.addScore(d.rollDie());
+        }
+
+        System.out.println(p1 + "\n" + p2 + "\n" + p3 + "\n" + p4);        
     }
 
 }
